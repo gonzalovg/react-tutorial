4819f20b716a5da74055cafd139988cbd5d32a12
GonzaloVG <gonzaloverdugog@gmail.com>
2021-08-12 18:03:24 +0200

    Eventos

--- test1/src/Components/Eventos.js
+++ test1/src/Components/Eventos.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 export default class Evento extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+    };
+
+    this.sumar = this.sumar.bind(this);
+    this.restar = this.restar.bind(this);
+  }
+  sumar(e) {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+  restar(e) {
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  }
   render() {
     return (
       <div>
         <h2>Eventos en React</h2>
+        <div>
+          <button onClick={this.sumar}>+</button>
+          <button onClick={this.restar}>-</button>
+        </div>
+        <h2>{this.state.contador}</h2>
       </div>
     );
   }
